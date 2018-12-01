8f42ce53b28aa59a8eb8a297aaf075e99ac2c51f
Bisvarup Mukherjee <codeworks0301@gmail.com>
2018-12-01 19:45:21 +0530

    git shared component to work

--- frontend/src/app/contacts/contacts.module.ts
+++ frontend/src/app/contacts/contacts.module.ts
@@ -6,10 +6,15 @@ import { ContactsRoutingModule } from "./contacts-routing.module";
 import { ContactsComponent } from "./contacts/contacts.component";
 import { InfoComponent } from "./info/info.component";
 import { MessageComponent } from "./message/message.component";
-import { CardComponent } from './info/card/card.component';
-
+import { CardComponent } from "./info/card/card.component";
+import { SharedModule } from "../shared/shared.module";
 @NgModule({
-  imports: [CommonModule, ContactsRoutingModule, FormsModule],
-  declarations: [ContactsComponent, InfoComponent, MessageComponent, CardComponent]
+  imports: [CommonModule, ContactsRoutingModule, FormsModule, SharedModule],
+  declarations: [
+    ContactsComponent,
+    InfoComponent,
+    MessageComponent,
+    CardComponent
+  ]
 })
 export class ContactsModule {}
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FooterComponent } from "./components/footer/footer.component";
+import { RouterModule } from "@angular/router";
 import { SharedRoutingModule } from "./shared-routing.module";
 import { ContactsService } from "./service/contacts.service";
+import { HeaderComponent } from "./components/header/header.component";
 
 @NgModule({
-  imports: [CommonModule, SharedRoutingModule],
-  declarations: [FooterComponent],
+  imports: [CommonModule, RouterModule],
+  declarations: [FooterComponent, HeaderComponent],
   providers: [ContactsService],
-  exports: [FooterComponent]
+  exports: [FooterComponent, HeaderComponent]
 })
 export class SharedModule {}
