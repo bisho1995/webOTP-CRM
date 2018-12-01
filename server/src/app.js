d0eac6f6cb83e188ad1c996fe5edf1305648ddb3
Bisvarup Mukherjee <codeworks0301@gmail.com>
2018-12-01 10:57:58 +0530

    initial server code

---
+++ server/src/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import path from "path";
+import logger from "morgan";
+import bodyParser from "body-parser";
+import routes from "./routes";
+
+const app = express();
+app.disable("x-powered-by");
+
+app.use(
+  logger("dev", {
+    skip: () => app.get("env") === "test"
+  })
+);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "../public")));
+
+// Routes
+app.use("/", routes);
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res) => {
+  // eslint-disable-line no-unused-vars
+  res.json({ err: err.message });
+});
+
+export default app;
