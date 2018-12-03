f3cf21c14312650abc001868e468036cb2f616f1
Bisvarup Mukherjee <codeworks0301@gmail.com>
2018-12-03 07:45:08 +0530

    improved some parts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,42 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { NgModule, Injectable, ErrorHandler } from "@angular/core";
+import * as Sentry from "@sentry/browser";
 
 import { SharedModule } from "./shared/shared.module";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 import { Error404Component } from "./error404/error404.component";
-import { SettingsComponent } from './settings/settings.component';
-import { AboutComponent } from './about/about.component';
-import { DocsComponent } from './docs/docs.component';
-import { PrivacyComponent } from './privacy/privacy.component';
+import { SettingsComponent } from "./settings/settings.component";
+import { AboutComponent } from "./about/about.component";
+import { DocsComponent } from "./docs/docs.component";
+import { PrivacyComponent } from "./privacy/privacy.component";
+
+Sentry.init({
+  dsn: "https://08499ce0ed6243449b6be9af5b820fcf@sentry.io/1335472"
+});
+
+@Injectable()
+export class SentryErrorHandler implements ErrorHandler {
+  constructor() {}
+  handleError(error) {
+    Sentry.captureException(error.originalError || error);
+    throw error;
+  }
+}
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, Error404Component, SettingsComponent, AboutComponent, DocsComponent, PrivacyComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    Error404Component,
+    SettingsComponent,
+    AboutComponent,
+    DocsComponent,
+    PrivacyComponent
+  ],
   imports: [BrowserModule, AppRoutingModule, SharedModule],
-  providers: [],
+  providers: [{ provide: ErrorHandler, useClass: SentryErrorHandler }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+
 import { FooterComponent } from "./components/footer/footer.component";
 import { HttpClientModule } from "@angular/common/http";
 import { RouterModule } from "@angular/router";
